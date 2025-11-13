c7a6828667be99d9a175d18808410136ba9271b3
kayzo1903 <kaykinangukammari@gmail.com>
2025-11-14 01:27:51 +0300

    cross orign co.tz

--- lib/auth-client.tsx
+++ lib/auth-client.tsx
@@ -3,7 +3,7 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
   baseURL: process.env.NODE_ENV === "production"
-    ? "https://zetutech.vercel.app"
+    ? "https://zetutech.co.tz"
     : "http://localhost:3000/api/auth",
 
   trustedOrigins: ["http://localhost:3000", "https://zetutech.vercel.app", "https://zetutech.co.tz"],
